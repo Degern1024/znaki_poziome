537703ceb91386ffe469f833dd48199539540a8e
Degern1024 <102228685+Degern1024@users.noreply.github.com>
2024-11-26 19:59:31 +0100

    Update streamlit.sh

--- streamlit.sh
+++ streamlit.sh
@@ -1,6 +1,7 @@
 sudo apt-get install python-matplotlib python-numpy python-pil python-scipy
 #pip install torch
-pip install ultralytics
+pip install opencv-python-headless
+pip install ultralytics==8.3.36
 pip install scikit-image
 pip install streamlit
 python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
